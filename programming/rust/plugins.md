969ffe7f7b15b0ca60a24ca96df9c842b8f8dd04
sanghaifa <sanghaifa@sensetime.com>
2021-11-03 17:59:22 +0800

    2020-11-3

--- programming/rust/plugins.md
+++ programming/rust/plugins.md
@@ -4,3 +4,8 @@
 [Plugins in Rust: Getting Started](https://nullderef.com/blog/plugin-start/#_abi_unstability_its_much_worse_than_it_seems)
 [Plugins in Rust](https://adventures.michaelfbryan.com/posts/plugins-in-rust/)
 [The (unofficial) Rust FFI Guide: Dynamic Loading & Plugins](https://michael-f-bryan.github.io/rust-ffi-guide/dynamic_loading.html)
+
+
+
+[Rust Plugins四篇文章](https://nullderef.com/series/rust-plugins/)
+* CNCF Sandox 项目, 使用的Actor Model 模型[Tremor](https://github.com/tremor-rs/tremor-runtime)
---
+++ programming/web/demo/scripts/main.js
@@ -0,0 +1,36 @@
+let myImage = document.querySelector("img");
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+    let myName = prompt('请输入你的名字');
+    // prompt弹出框 什么都不填写点击确认 myName===''
+    // 点击取消的话, 就是null
+    if (!myName || myName === null) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+    }
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+
+myButton.onclick = function() {
+    setUserName();
+}
